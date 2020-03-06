e5d9f752d536d786d0e99930fdd18a7157851511
Vyacheslav Borodkin <slavagsm13@mail.ru>
2020-03-06 11:53:09 +0300

    Base for application

---
+++ MVVMKotlinCoroutineRetrofitDemo-master/app/src/main/java/com/example/mvvmkotlincoroutineretrofitdemo/view/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mvvmkotlincoroutineretrofitdemo.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mvvmkotlincoroutineretrofitdemo.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+
+
+}
